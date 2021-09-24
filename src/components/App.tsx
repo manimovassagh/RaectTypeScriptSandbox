c46611dda36e68709d8ad3663abdffa10e59389e
manimovassagh <mani.mgh@gmail.com>
2021-09-24 14:35:49 +0200

    1

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import React, { ReactElement } from 'react';
+import { Header } from "./Header"
+import {Menu} from './Menu'
+
+
+
+export default function App(): ReactElement {
+
+  return (
+
+    <div>
+      <Menu/>
+    <Header />
+    Just check
+    </div>
+
+
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react'
+
+export function Header(): ReactElement {
+    return (
+        <div>
+            <h1 >Just to have a header</h1>
+        </div>
+    )
+}
